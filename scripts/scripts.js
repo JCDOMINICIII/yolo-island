be812afca956ebb0db5b43e2b882756d9c6dd66d
Ibebuike <ibebuikejethro@gmail.com>
2026-07-29 21:48:18 +0100

    Initial upload of Yolo Island website

---
+++ scripts/scripts.js
@@ -0,0 +1,37 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('active');
+  navLinks.classList.toggle('active');
+});
+
+const hiddenElements = document.querySelectorAll('feature-card');
+hiddenElements.forEach((card) => {
+  card.classList.add('hidden');
+});
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+});
+
+hiddenElements.forEach((card) => observer.observe(card))
+
+const navbar = document.querySelector(".navbar");
+window.addEventListener("scroll", () => {
+
+if(window.scrollY > 50){
+
+navbar.classList.add("scrolled");
+
+}else {
+
+navbar.classList.remove("scrolled");
+
+}
+
+});
